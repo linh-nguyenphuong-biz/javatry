f14204b1c18aa958f20519c3a1139e0c3e839658
Linh Nguyen Phuong <linh.nguyenphuong@bizreach.co.jp>
2020-11-17 11:08:08 +0900

    Step 22

--- src/main/java/org/docksidestage/bizfw/debug/searcher/IteratorSearcher.java
+++ src/main/java/org/docksidestage/bizfw/debug/searcher/IteratorSearcher.java
@@ -20,8 +20,9 @@ public class IteratorSearcher implements Searcher {
     public Word search(String searchingFor) {
         Iterator<Word> iterator = words.iterator();
         while (iterator.hasNext()) {
-            if (iterator.next().getWord().equals(searchingFor)) {
-                return iterator.next();
+            Word next = iterator.next();
+            if (next.getWord().equals(searchingFor)) {
+                return next;
             }
         }
         throw new IllegalArgumentException("the word you are looking for is not here, word:" + searchingFor);
